bfd83f2c30c84808efe18f101110d9c076ed67ca
AvinashKumarPaladi <88394644+AvinashKumarPaladi@users.noreply.github.com>
2026-03-12 12:07:31 +0530

    Add setup.py for package configuration

---
+++ setup.py
@@ -0,0 +1,12 @@
+# from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(name='modifiedPulsePortraiture',
+      version='1.0',
+      description='Data analysis package for wideband and multiband wideband pulsar timing with MLA and MLAN technique for InPTA',
+      author='Avinash Kumar Paladi',
+      author_email='avinashkumarpaladi@gmail.com',
+      url='https://github.com/AvinashKumarPaladi/modifiedPulsePortraiture',
+      py_modules=[ 'mpplib', 'mpptoas', 'mpptoaslib', 'mpp_telescope_codes', 'mpplib_b35', 'mpptoas_b35', 'mpptoaslib_b35', 'mpptoaslib_MLAN', 'mpptoaslib_b35_MLAN', 'mpp_gaussian_test'],
+      scripts=['mpptoas.py', 'mpptoas_b35.py']
+     )
